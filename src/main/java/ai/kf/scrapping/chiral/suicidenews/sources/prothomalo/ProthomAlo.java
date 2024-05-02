e41cab88ee924d93e68c751d5ce0aad60a20bbf1
KAIF <mibnzaman@outlook.com>
2024-05-03 01:15:54 +0600

    Source Updated

--- src/main/java/ai/kf/scrapping/chiral/suicidenews/sources/prothomalo/ProthomAlo.java
+++ src/main/java/ai/kf/scrapping/chiral/suicidenews/sources/prothomalo/ProthomAlo.java
@@ -10,17 +10,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class ProthomAlo {
     private static final Logger log = LogManager.getLogger(ProthomAlo.class);
 
     public static void main(String[] args) throws IOException {
-        String url = "https://en.prothomalo.com/api/v1/advanced-search?section-id=16600%2C16601%2C16603%2C16604%2C16605%2C16606%2C16607%2C16610%2C16619%2C16622%2C16623%2C16624%2C16725%2C16727%2C16728%2C16739%2C16740%2C16741%2C16742%2C16743%2C16744%2C16745%2C16746%2C16747%2C16748%2C16751%2C16752%2C16756%2C16762%2C16763%2C16764%2C16765%2C16766%2C16767%2C16768%2C16770%2C16771%2C16772%2C16773%2C16774%2C16775%2C16776%2C16781%2C17111%2C17129%2C17130%2C17131%2C17134%2C17135%2C17136%2C17137%2C17138%2C17139%2C17140%2C17143%2C17144%2C17202%2C35627%2C35628%2C35629%2C35639%2C35640&q=heat+stroke&sort=latest-published&offset=35&limit=50&fields=headline%2Csubheadline%2Cslug%2Curl%2Ctags%2Chero-image-s3-key%2Chero-image-caption%2Chero-image-metadata%2Clast-published-at%2Calternative%2Cauthors%2Cauthor-name%2Cauthor-id%2Csections%2Cstory-template%2Cmetadata%2Chero-image-attribution%2Caccess";
+        int limit = 600;
+        String url = "https://en.prothomalo.com/api/v1/advanced-search?section-id=16600%2C16725%2C16727%2C16728%2C17129%2C17134%2C17135%2C17136%2C17137%2C17139%2C35627&q=heat+stroke&offset=0&limit="+limit+"&fields=headline%2Csubheadline%2Cslug%2Curl%2Ctags%2Chero-image-s3-key%2Chero-image-caption%2Chero-image-metadata%2Clast-published-at%2Calternative%2Cauthors%2Cauthor-name%2Cauthor-id%2Csections%2Cstory-template%2Cmetadata%2Chero-image-attribution%2Caccess";
         String res = getJsonRes(url);
-        System.out.println(res);
         Gson gson = new Gson();
         News news = gson.fromJson(res, News.class);
-        System.out.println(news.getItems().get(0).getHeadline());
+
+
+        ArrayList<News.Item> expected = new ArrayList<>();
+        ArrayList<News.Item> items = (ArrayList<News.Item>) news.getItems();
+
+
+        for (News.Item item : items) {
+//            if (item.getHeadline().toLowerCase().contains("heat") ||
+//                item.getHeadline().toLowerCase().contains("stroke") ||
+//                item.getHeadline().toLowerCase().contains("death") ||
+//                item.getHeadline().toLowerCase().contains("dead") ||
+//                item.getHeadline().toLowerCase().contains("died") ||
+//                item.getHeadline().toLowerCase().contains("stress")
+//            ) {
+//                expected.add(item);
+//            }
+            ArrayList<News.Tag> tags= (ArrayList<News.Tag>) item.getTags();
+            for (News.Tag tag:tags) {
+                if(!tag.getName().contains("heat")) return;
+            }
+            for (News.Tag tag: tags) {
+                if(!tag.getName().contains("death")) return;
+            }
+            expected.add(item);
+        }
+
+        int count = 0;
+        for (News.Item item : expected) {
+            count++;
+            System.out.println(count+ " "+item.getHeadline());
+        }
     }
 
 
